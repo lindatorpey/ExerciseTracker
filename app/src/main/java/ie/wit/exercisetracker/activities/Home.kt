4bdf046f1d456c1ac7085d1ce831b25164614b53
lindatorpey <45608202+lindatorpey@users.noreply.github.com>
2022-02-13 12:59:46 +0000

    fragments filled

---
+++ app/src/main/java/ie/wit/exercisetracker/activities/Home.kt
@@ -0,0 +1,12 @@
+package ie.wit.exercisetracker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.wit.exercisetracker.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
