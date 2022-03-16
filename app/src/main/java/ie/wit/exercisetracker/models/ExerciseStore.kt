3f76ed6b6ba4aac3555ed944ab34f75bdac13fca
lindatorpey <45608202+lindatorpey@users.noreply.github.com>
2022-03-16 12:32:58 +0000

    TriTrack refactor
    Changing from excercise tracker to TriTrack

--- app/src/main/java/ie/wit/exercisetracker/models/ExerciseStore.kt
+++
@@ -1,7 +0,0 @@
-package ie.wit.exercisetracker.models
-
-interface ExerciseStore {
-    fun findAll() : List<ExerciseModel>
-    fun findById(id: Long) : ExerciseModel?
-    fun create(exercise: ExerciseModel)
-}
